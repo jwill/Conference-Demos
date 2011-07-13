54ce185c08199c19c5843803b5408022cdb91d18
James Williams <james.l.williams@gmail.com>
2011-07-13 00:34:42 -0700

    Created new project for SF Grails preso.

---
+++ user-groups/sfgrails-jul-2011/blog/src/main/groovy/BlogApp.groovy
@@ -0,0 +1,48 @@
+import com.bleedingwolf.ratpack.*
+import jwill.deckchair.*
+import org.json.*
+
+class BlogApp {
+	def	app = Ratpack.app {
+				set 'public', 'public'
+				set 'templateRoot', 'templates'
+				
+				def derby = new Deckchair([name:'posts',adaptor:'derby'])
+				
+				get("/") {
+					def list = derby.all()
+					render '/entry/index.html', [posts: list, contextPath:request.getContextPath()]
+				}
+				
+				get("/entry/list") {
+					def list = derby.all()
+					render '/entry/index.html', [posts: list, contextPath:request.getContextPath()]
+				}
+
+				post("/entry/save") {
+					def entry = new Entry(params)
+					derby.save(entry.properties, {obj ->
+						println "Finished saving."
+						new JSONObject([success:true]).toString()
+					})
+				}
+
+				get("/entry/create") {
+					render '/entry/create.html', [contextPath:request.getContextPath()]
+				}
+
+				get("/entry/show/:id") {
+					def entry = derby.get(urlparams.id)
+					render '/entry/show.html', [entry: entry, contextPath:request.getContextPath()]
+				}
+
+		}
+		public BlogApp() {
+			AuthHelper.doAuth(app)
+			RatpackServlet.serve(app)	
+		}
+		
+		public static void main(String[] args) {
+			new BlogApp()
+		}
+}
