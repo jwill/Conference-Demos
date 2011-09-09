fefa1083db52d0524cade4bf942edf7b56c4ce3c
James Williams <james.l.williams@gmail.com>
2011-09-09 16:43:27 -0700

    Tweaks to blog app.

--- user-groups/sfgrails-jul-2011/blog/src/main/groovy/BlogApp.groovy
+++ user-groups/sfgrails-jul-2011/blog/src/main/groovy/BlogApp.groovy
@@ -11,7 +11,7 @@ class BlogApp {
 				
 				get("/") {
 					def list = derby.all()
-					render '/entry/index.html', [posts: list, contextPath:request.getContextPath()]
+					render '/entry/index.html', [posts: list.reverse(), contextPath:request.getContextPath()]
 				}
 				
 				get("/entry/list") {
@@ -34,6 +34,18 @@ class BlogApp {
 				get("/entry/show/:id") {
 					def entry = derby.get(urlparams.id)
 					render '/entry/show.html', [entry: entry, contextPath:request.getContextPath()]
+				}
+				
+				get("/entry/delete/:id") {
+					derby.remove(urlparams.id)
+				}
+				
+				get("/archive") {
+					
+				}
+				
+				get("/search") {
+					
 				}
 
 		}
