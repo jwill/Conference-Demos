c91ea614f5a40e23c8073059747e8685c86c2ad9
James Williams <james.l.williams@gmail.com>
2011-09-23 20:59:32 -0700

    HTML5 Dev Conf preso update.

---
+++ user-groups/html5-dev-conf-2011/presentation/demos/geom.js
@@ -0,0 +1,68 @@
+var drawTriangle = (function() {
+var camera, scene, renderer;
+
+function init() {
+	// scene size
+	var HEIGHT = 200
+	, WIDTH = 200;
+	
+	// camera properties
+	var FOV = 45
+	, ASPECT = WIDTH/HEIGHT
+	, NEAR = 0.1
+	, FAR = 10000;
+	
+	// create a renderer, camera, and scene
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize (WIDTH, HEIGHT);
+	camera = new THREE.Camera(FOV, ASPECT, NEAR, FAR);
+	camera.position.z = 25;
+	//camera.position.y = 30;
+	
+	scene = new THREE.Scene();
+	drawTriangle();
+	
+	// attach the scene to the DOM
+	$("#shader-demo").get(0).appendChild(renderer.domElement);
+}
+
+function drawTriangle() {
+	var geometry, geoMaterial;
+	
+	geoMaterial = new THREE.MeshLambertMaterial({
+			color:0xFF00FF
+	});
+	shaderMaterial = new THREE.MeshShaderMaterial({
+			vertexShader: $('#geom-vertexShader').get(0).innerHTML,
+			fragmentShader: $('#geom-fragmentShader').get(0).innerHTML,
+			vertexColors: true
+	});
+	
+	
+	geometry = new THREE.Geometry();
+	geometry.vertices.push(new THREE.Vertex(new THREE.Vector3(0, 10, 0)));
+	geometry.vertices.push(new THREE.Vertex(new THREE.Vector3(-10, -10, 0)));
+	geometry.vertices.push(new THREE.Vertex(new THREE.Vector3(10, -10, 0)));
+	geometry.faces.push(new THREE.Face3(0,1,2));
+	var triangle = new THREE.Mesh(geometry, shaderMaterial);
+		
+	plane = new THREE.Mesh( new THREE.Plane( 200, 200 ), new THREE.MeshBasicMaterial( { color: 0xe0e0e0 } ) );
+	//plane.rotation.x = - 90 * ( Math.PI / 180 );
+	plane.overdraw = true;
+	scene.addObject( plane );
+	
+	scene.addChild(triangle);
+}
+
+function animate() {
+	requestAnimationFrame (animate);
+	render();
+}
+
+function render() {
+	renderer.render(scene, camera);
+}
+
+init();
+animate();
+})();
